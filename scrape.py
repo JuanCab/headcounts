b9efd8c7d6461dc81f7e336839c87f6e91b41d92
Matt Craig <mattwcraig@gmail.com>
2014-08-11 23:41:50 -0500

    Add function to scrape detail data for one course

--- scrape.py
+++ scrape.py
@@ -4,6 +4,7 @@ import lxml.html
 
 URL_ROOT = 'https://webproc.mnscu.edu/registration/search/basic.html?campusid=072'
 SUBJECT_SEARCH_URL = 'https://webproc.mnscu.edu/registration/search/advancedSubmit.html?campusid=072&searchrcid=0072&searchcampusid=072&yrtr=20153&subject={subj}&courseNumber=&courseId=&openValue=ALL&showAdvanced=&delivery=ALL&starttime=&endtime=&mntransfer=&gened=&credittype=ALL&credits=&instructor=&keyword=&begindate=&site=&resultNumber=250'
+COURSE_DETAIL_URL = 'https://webproc.mnscu.edu/registration/search/detail.html?campusid=072&courseid={course_id}&yrtr=20153&rcid=0072&localrcid=0072&partnered=false&parent=search'
 
 
 def get_subject_list():
@@ -34,11 +35,39 @@ def class_list_for_subject(subject):
     lxml_parsed = lxml.html.fromstring(result.text)
     #print lxml.html.tostring(lxml_parsed)
     foo = lxml_parsed.findall('.//tbody/tr/td[2]')
-    print len(foo)
     course_ids = [f.text.strip() for f in foo]
     return course_ids
+
+
+def course_detail(cid):
+    def parse_size_cap(element):
+        return int(element.getparent().text_content().split(':')[1].strip())
+
+    course_url = COURSE_DETAIL_URL.format(course_id=cid)
+    result = requests.get(course_url)
+    #print result.text
+    lxml_parsed = lxml.html.fromstring(result.text)
+
+    detail_table_header = lxml_parsed.xpath('.//table[@summary="Course Detail"]//th//text()')
+    print detail_table_header
+    detail_table_data = lxml_parsed.xpath('.//table[@summary="Course Detail"]//td')
+    for d in detail_table_data[1:]:
+        print d.text_content().strip()
+    for h, d in zip(detail_table_header, detail_table_data[1:]):
+        print h.strip(), ' '.join(d.text_content().strip().split())
+
+    xpath_expr = './/*[contains(text(), $key)]'
+    to_get = {'Size': None, 'Enrolled': None}
+    for key in to_get.keys():
+        foo = lxml_parsed.xpath(xpath_expr, key=key)
+        to_get[key] = parse_size_cap(foo[0])
+    print cid, to_get
+
 
 if __name__ == '__main__':
     subjects = get_subject_list()
     #print "Trying {}".format(subjects[0])
     IDs = class_list_for_subject('ART')
+    for an_id in IDs[0:1]:
+        print "On ID: {}".format(an_id)
+        course_detail(an_id)
